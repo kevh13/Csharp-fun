baec1a151c044ca0c3ed1b833d3d9f65ea1b9a2c
unknown <blue8@RM02-08.fasfg.local>
2015-06-22 14:00:29 +0100

    fourth2

---
+++ Lesson4c/Switch/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch(listBox1.SelectedItem.ToString())
+            {
+                case "Foobar":
+                    MessageBox.Show("You picked Foobar");
+                    break;
+                case "Bazquirk":
+                    MessageBox.Show("You picked Bazquirk");
+                    break;
+
+                default:
+                    MessageBox.Show("You picked something else");
+                    break;
+            }
+        }
+    }
+}

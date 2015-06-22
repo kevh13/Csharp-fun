f4e718b2398898192b1641941b579115237849ab
unknown <blue8@RM02-08.fasfg.local>
2015-06-22 11:23:55 +0100

    third

---
+++ Lesson3/Calculator/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int firstTextBox = 0;
+            int secondTextBox = 0; 
+            int result = 0; 
+            
+            firstTextBox = int.Parse(textBox1.Text); 
+            secondTextBox = int.Parse(textBox2.Text); 
+            result = firstTextBox + secondTextBox; label3.Text = result.ToString();
+        }
+    }
+}
